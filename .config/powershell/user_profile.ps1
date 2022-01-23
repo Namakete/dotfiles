b751dbcd41d7b0102b6da9fabadbd98eae24cc97
Ilya Oberemok <namakete.dev@gmail.com>
2022-01-23 05:05:40 +0300

    Upd

---
+++ .config/powershell/user_profile.ps1
@@ -0,0 +1,24 @@
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme hunk
+Import-Module Terminal-Icons
+
+
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
