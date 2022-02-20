21038bc22c36fa99e3c14097672f92865fab40da
Ilya Oberemok <namakete.dev@gmail.com>
2022-02-21 02:19:47 +0300

    upd

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
-require "user.configuration"
-require "user.keymaps"
-require "user.packer"
-require "user.nerdtree"
-require "user.theme"
-require "user.telescope"
-require "user.cmp"
-require "user.lsp"
-require "user.autopairs"
+require('user.configuration')
+require('user.keymaps')
+require('user.packer')
+require('user.nerdtree')
+require('user.theme')
+require('user.telescope')
+require('user.cmp')
+require('user.lsp')
+require('user.autopairs')
+require('user.lspconfig')
--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -14,3 +14,14 @@ return {
 		},
 	},
 }
+
+local servers = { 'pyright', 'fortls', 'bashls' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+     capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+  end
