15609f66c59c1d841e735b3958b251425316c743
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-02 19:14:18 +0300

    upd

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -9,14 +9,8 @@ M.setup = function()
   { name = "DiagnosticSign", text = "﫠"},
   }
 
-
   for type, sign in pairs(signs) do
     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-  end
-
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
   end
 
   local config = {
