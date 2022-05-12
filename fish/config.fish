78c19382ecae6051de2b8a627559e52662d9b1f4
Ilya Oberemok <namakete.dev@gmail.com>
2022-05-12 22:23:28 +0300

    Added new 'termcolors' script

--- fish/config.fish
+++ fish/config.fish
@@ -12,14 +12,20 @@ set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
+
+# Show list data
 alias ls "ls -p -G"
 alias la "ls -A"
 alias ll "exa --long --git --icons"
 alias lla "ll -a"
+
+# Git
 alias g git
 alias lg lazygit
-alias gu gitui
-alias cls clear
+
+# Bind vim & neovim
 command -qv nvim && alias vim nvim
 
+# Bind scripts path
 alias covid "~/.config/scripts/covid"
+alias termcolors "~/.config/scripts/termcolors"
