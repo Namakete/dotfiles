e4b79430c1810be0316506c440f32b2dbd18a139
Ilya Oberemok <namakete.dev@gmail.com>
2023-12-26 21:21:20 +0300

    New nvim config with nvimchad

--- nvim/lua/user/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd [[colorscheme solarized-osaka]]
-
-vim.api.nvim_exec([[
-augroup MyColors
-  autocmd!
-  autocmd ColorScheme * hi MatchParen guibg=NONE guifg=darkred ctermfg=darkred cterm=NONE
-augroup END
-]], false)
